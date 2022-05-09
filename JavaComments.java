ef8057c4764c75c47baa3207a3bba6a5bf4d28cb
Gabriel Steven Machicao Quispe <gmachicaoq@unsa.edu.pe>
2022-05-09 12:12:59 -0500

    Se cambia la impresión final, con el resultado de los operadores dados en el println.

--- JavaComments.java
+++ JavaComments.java
@@ -1,8 +1,9 @@
-import java.util.Scanner;
+import java.util.Scanner;//Se introduce la clase Scanner
 public class JavaComments {
   public static void main(String[] args) {
     /* Los comentarios sirven para añadir explicación al código realizado
      Haciendolo más legible*/
+
     //Declaración de variables
     int num1, num2;
     Scanner sc = new Scanner(System.in);
@@ -14,8 +15,9 @@ public class JavaComments {
     //Se hace lo mismo con la variable num2
     System.out.println("Número 2: ");
     num2= sc.nextInt();
-    //Impresión de datos 
-    System.out.println("Número 1: " + num1 + " y Número 2: " + num2);  
+    //Impresión de resultado de las operaciones 
+    System.out.println("La suma es: " + (num1 + num2));  
+    System.out.println("La resta es: " + (num1 - num2));
   }
 }
 
