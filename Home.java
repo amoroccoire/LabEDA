ed0fd54510876efeb6bef8062e2909498db64eb4
Diego Gabriel Escobar Atamari <descobara@unsa.edu.pe>
2022-05-10 09:38:50 -0500

    Imprimiendo mensaje en java

---
+++ Home.java
@@ -0,0 +1,15 @@
+public class Main {
+  public static void main (String[]args){
+     System.out.print("Comentario para la prueba")
+     System.out.print("Ejecutando el programa")
+     System.out.print("Muestra de texto")
+
+     System.out.print("Imprimiendo texto")
+     System.out.print("Imprimiendo texto")
+     System.out.print("Imprimiendo texto")
+     System.out.print("Imprimiendo texto")
+     System.out.print("Imprimiendo texto")
+     System.out.print("Imprimiendo texto")
+     System.out.print("Se termino")
+ }  
+}
