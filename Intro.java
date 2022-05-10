92b5432471368d01be535de697eccd294f961844
Diego Gabriel Escobar Atamari <descobara@unsa.edu.pe>
2022-05-10 11:30:45 -0500

    Correccion en los saltos de linea

--- Intro.java
+++ Intro.java
@@ -1,11 +1,11 @@
-public class Main {
+public class Intro {
   public static void main (String[]args){
-     System.out.print("Java es un popular lenguaje, creado en 1995\n es propiedad de Oracle, y mas de 3 millones de dispositivos usan Java.  ")
-     System.out.print("Es usado para: \n
-                       *Aplicaciones moviles\n
-                       *Aplicaciones web\n
-                       *Videojuegos\n
-                       *Coneccion a base de datos
-                       *Aplicaciones de escritorio")
+     System.out.print("Java es un popular lenguaje, creado en 1995\n es propiedad de Oracle, y mas de 3 millones de dispositivos usan Java.");
+     System.out.print("Es usado para: \n"+
+                       "*Aplicaciones moviles\n"+
+                       "*Aplicaciones web\n"+
+                       "*Videojuegos\n"+
+                       "*Coneccion a base de datos\n"+
+                       "*Aplicaciones de escritorio");
   }
 } 
